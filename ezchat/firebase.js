a264bee0f564072daf52034faa75263404c5df13
Ishan-sa <ishansach19@gmail.com>
2022-06-06 13:32:18 +0530

    firebase error default already exists

--- ezchat/firebase.js
+++ ezchat/firebase.js
@@ -1,6 +1,11 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAuth } from 'firebase/auth'
+import firebase from "firebase/app";
+import { getAuth } from 'firebase/auth';
+
+
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { useCollectionData } from 'react-firebase-hooks/firestore';
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -18,5 +23,7 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
+// const auth = firebase.auth();
+
 
 export { auth };
